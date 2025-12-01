06b1cfbea12ccc6127592b3c101d35ad93e4ab28
Sandesh Rauth <sandeshrauth644@gmail.com>
2025-12-01 07:16:03 -0800

    Time: 10 ms (87.76%) | Memory: 58.5 MB (88.36%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.js
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.js
@@ -5,21 +5,22 @@
  */
 var exclusiveTime = function (n, logs) {
     const stack = [];
-    let prevTime = 0;
+    let prevTime = 0; //tracks previous time. 
     const result = new Array(n).fill(0);
     for (let log of logs) {
-        const sp = log.split(":");
+        const sp = log.split(":"); //breaks strings
         if (sp[1] === 'start') {
+            //checks if stack actually has elements or not. 
             if (stack.length > 0) {
-                const pt = Number(sp[2]) - prevTime;
-                result[stack[stack.length - 1]] += pt
+                const pt = Number(sp[2]) - prevTime; // calculates the current executing time of previous function though not ended. 
+                result[stack[stack.length - 1]] += pt //adds time to the result. 
             }
-            stack.push(Number(sp[0]));
-            prevTime = Number(sp[2]);
+            stack.push(Number(sp[0])); // push curren id to stack since it is start. 
+            prevTime = Number(sp[2]); // prevTime is just the current start time. 
         } else {
-            result[Number(sp[0])] += Number(sp[2]) - prevTime + 1;
+            result[Number(sp[0])] += Number(sp[2]) - prevTime + 1; // calculates and updates total execution time. 
             stack.pop();
-            prevTime = Number(sp[2]) + 1;
+            prevTime = Number(sp[2]) + 1; // If function ends on certain timestamp, then start time for subsequent function is always +1. 
         }
     }
 
